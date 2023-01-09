c1399a404db1d76b7b077265c2494243c544b438
Sn1cke <113979281+Sn1cke@users.noreply.github.com>
2023-01-09 15:24:09 +0100

    Updated scientist DB files

--- src/app.js
+++ src/app.js
@@ -1,5 +1,37 @@
-var o = {
-    x: 12,
-    y: "hej"
+var evilScientistList = {
+    evilScientists: [
+        {
+            nickname: "Victor Frankenstein",
+            age: 37,
+            henchmen: 7,
+            description: "Likes to write poetry."
+        },
+        {
+            nickname: "Alfred Nobel",
+            age: 26,
+            henchmen: 2,
+            description: "Killed his brother in a factory accident."
+        },
+    ],
+    loadScientists: function (arr) {
+        arr.forEach(function (scientist) {
+            var scientistList = document.querySelector('.scientist-list');
+            var scientistCard = document.createElement('section');
+            scientistCard.className = 'scientist-card';
+            scientistList.append(scientistCard);
+            var scientistName = document.createElement('h2');
+            scientistName.textContent = scientist.nickname;
+            scientistCard.append(scientistName);
+            var scientistAge = document.createElement('p');
+            scientistAge.textContent = "".concat(scientist.nickname, " is ").concat(scientist.age, " years old");
+            scientistCard.append(scientistAge);
+            var henchmen = document.createElement('p');
+            henchmen.textContent = "They have ".concat(scientist.henchmen, " henchmen working for them.");
+            scientistCard.append(henchmen);
+            var description = document.createElement('p');
+            description.textContent = scientist.description;
+            scientistCard.append(description);
+        });
+    }
 };
-console.log(o);
+evilScientistList.loadScientists(evilScientistList.evilScientists);
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,45 @@
-let o: {
-    x: number,
-    y: string,
-} = {
-    x: 12,
-    y: "hej",
+
+const evilScientistList = {
+    evilScientists: [
+        {
+            nickname: "Victor Frankenstein",
+            age: 37,
+            henchmen: 7,
+            description: "Likes to write poetry."
+        },
+        {
+            nickname: "Alfred Nobel",
+            age: 26,
+            henchmen: 2,
+            description: "Killed his brother in a factory accident."
+        },
+    ],
+    loadScientists: function (arr) {
+        arr.forEach(scientist => {
+            const scientistList = document.querySelector('.scientist-list') as HTMLElement;
+
+            const scientistCard = document.createElement('section')
+            scientistCard.className = 'scientist-card';
+            scientistList.append(scientistCard)
+
+            const scientistName = document.createElement('h2')
+            scientistName.textContent = scientist.nickname;
+            scientistCard.append(scientistName);
+
+            const scientistAge = document.createElement('p')
+            scientistAge.textContent = `${scientist.nickname} is ${scientist.age} years old`;
+            scientistCard.append(scientistAge)
+
+            const henchmen = document.createElement('p')
+            henchmen.textContent = `They have ${scientist.henchmen} henchmen working for them.`;
+            scientistCard.append(henchmen)
+
+            const description = document.createElement('p')
+            description.textContent = scientist.description;
+            scientistCard.append(description)
+        });
+    }
 }
 
-console.log(o);
+
+evilScientistList.loadScientists(evilScientistList.evilScientists);
