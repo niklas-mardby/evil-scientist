cf9c45c14c383c058f0b2329246da1b092629fe4
Niklas Mårdby <niklas.mardby@zocom.se>
2023-01-09 14:58:51 +0100

    More demo code

--- src/demo.ts
+++ src/demo.ts
@@ -14,3 +14,73 @@ type Hobby = string | undefined;
 type NumberOfSomething = 50 | 100;
 let y: NumberOfSomething = 50;
 
+
+type Monster = {
+    name: string,
+    arms: number,
+}
+
+const arrMonster: Monster[] = [
+    {
+        name: "Bob",
+        arms: 12
+    },
+    {
+        name: "Karen",
+        arms: 3
+    }
+];
+
+function createMonster(name: string, arms: number): Monster {
+    const m: Monster = {
+        name: name,
+        arms: arms
+    }
+    return m;
+}
+
+arrMonster.push(createMonster("Nisse", 13));
+
+type Hero = {
+    name: string,
+    hp: number,
+    takeDamage: (damage: number) => void
+}
+
+let h: Hero = {
+    name: "Gugge",
+    hp: 100,
+    takeDamage: (damage: number) => {
+        h.hp -= damage;
+    }
+}
+
+let monsterDB: {
+    monsters: Monster[],
+    addMonster: (n: string, a: number) => void
+} = {
+    monsters: [
+        {
+            name: "Bob",
+            arms: 12,
+        },
+        {
+            name: "Karen",
+            arms: 3
+        }
+    ],
+    addMonster: function (name: string, arms: number): void {
+        const m: Monster = {
+            name: name,
+            arms: arms
+        }
+        this.monsters.push(m);
+    }
+}
+
+console.log(monsterDB.monsters);
+
+monsterDB.addMonster("Benny", 33);
+
+console.log(monsterDB.monsters);
+
