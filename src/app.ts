d7daab6dc3dbc5467504464c72e0234bb83841ce
Niklas Mårdby <niklas.mardby@zocom.se>
2023-01-10 15:01:04 +0100

    Clean up of DOM code examples

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 // const pElement: HTMLParagraphElement | null = document.querySelector("p");
-const sElement = document.querySelector("section") as HTMLElement;
-
 // const pElement: HTMLParagraphElement = <HTMLParagraphElement>document.querySelector("p");
-
+const sElement = document.querySelector("section") as HTMLElement;
 sElement.append("test");
 
+const iElement = document.querySelector("#evilScientistName") as HTMLInputElement;
+iElement.value = "value";
