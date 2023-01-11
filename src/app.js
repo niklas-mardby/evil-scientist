9db3d5dfd250bb9ebf9f4908feee3b8a7a9ab3e0
Niclas Broberg <niclas-92@live.se>
2023-01-11 11:02:39 +0100

    New code in TS

--- src/app.js
+++ src/app.js
@@ -16,13 +16,28 @@ const arrScientists: scientists[] = [
 
 const numberArr: number[] = [1, "x", 3, 4];
 */
-function addScientist(name, age, henchmen, description) {
-    var test = {
-        name: name,
-        age: age,
-        henchmen: henchmen,
-        description: description
-    };
-    return test;
-}
-// change
+var scientistDB = {
+    scientists: [
+        {
+            name: "Victor Frankenstein",
+            age: 37,
+            henchmen: 2,
+            description: "Likes to write poetry"
+        },
+        {
+            name: "Alfred Nobel",
+            age: 26,
+            henchmen: 1,
+            description: "Killed his brother in a factory accident"
+        }
+    ],
+    addScientist: function (name, age, henchmen, description) {
+        var x = {
+            name: name,
+            age: age,
+            henchmen: henchmen,
+            description: description
+        };
+        this.scientists.push(x);
+    }
+};
